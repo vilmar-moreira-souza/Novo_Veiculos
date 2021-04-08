1ca945b28fefb30be5164150457fb44b04da6df7
vilmar <vilmarmoreira2010@gmail.com>
2021-04-08 00:45:04 -0300

    melhorias responsividade

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-script.js
+"use strict"
+
+const bMenu = document.querySelector("#bMenu")
+const logo = document.querySelector("#bLogo")
+const verMenu = document.querySelector("ul")
+
+
+function menu() {
+    const barra = document.querySelector(".barra")
+    if (verMenu.style.display == "block") {
+        verMenu.style.display = "none"
+        //bMenu.style.marginTop= "0"
+        //barra.style.marginTop = "0px"
+        logo.style.display = "flex"
+    } else {
+        verMenu.style.display = "block"
+        logo.style.display = "none"
+        verMenu.style.marginTop = "0px"
+    }
+    //bMenu.style.display = "flex"
+
+
+}
+
+bMenu.addEventListener("click", menu)
